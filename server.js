6e20e9cb84512839e96e706896b487cb81f98152
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2022-04-11 15:57:41 -0400

    sub

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use( (req, res, next) => {
        console.log('Your database appears to be empty. I will initialize it now.');
         // Set a const that will contain your SQL commands to initialize the database.
        const sqlInit = `
-       CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time DATE, method TEXT, protocall TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT);
+       CREATE TABLE accesslog (remoteaddr TEXT, remoteuser TEXT, time DATE, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT);
     `;
         // Execute SQL commands that we just wrote above.
        db.exec(sqlInit);
