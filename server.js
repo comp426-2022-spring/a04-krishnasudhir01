ba1ab8938655c48fb9537d6840c4f9ae1673986f
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2022-04-15 15:16:35 -0400

    sub5

--- server.js
+++ server.js
@@ -73,9 +73,14 @@ app.use( (req, res, next) => {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
-    const sqlInit = `
-        INSERT INTO accesslog(remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (${logdata.remoteaddr}, ${logdata.remoteuser}, ${logdata.time}, ${logdata.method}, ${logdata.url}, ${logdata.protocol}, ${logdata.httpversion}, ${logdata.status}, ${logdata.referer}, ${logdata.useragent});
-    `;
+  //  const sqlInit = `
+//        INSERT INTO accesslog(remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (${logdata.remoteaddr}, ${logdata.remoteuser}, ${logdata.time}, ${logdata.method}, ${logdata.url}, ${logdata.protocol}, ${logdata.httpversion}, ${logdata.status}, ${logdata.referer}, ${logdata.useragent});
+ //   `;
+
+const sqlInit = 'INSERT INTO accesslog(remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?)'
+db.run(sqlInit, [logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent])   
+
+
     // Execute SQL commands that we just wrote above.
     console.log('SQL' + sqlInit);
     //db.exec(sqlInit);
